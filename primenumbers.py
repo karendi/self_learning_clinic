28d84ab4754a81168cb0e5466bd1d180b095241c
Sharonnjihia <sharonkarendi16@gmail.com>
2016-11-30 22:20:28 +0300

    fixed the prime numbers generator

--- primenumbers.py
+++ primenumbers.py
@@ -1,10 +1,15 @@
 #the function that generates the primenumbers for numbers in a given range
 def prime_numbers(n):
     primes = []
-    for x in range(2 , n+1):
-        for i in range(2 , x+1):
-            if x % i == 0:
-                break
-        else:
-            primes.append(x)
-    return primes
+    if  n < 0:
+        return("You cannot enter negative numbers")
+    else:
+        for x in range(2 , n+1):
+            for i in range(2 , x):
+                if x % i == 0:
+                    break
+            else:
+                primes.append(x)
+        return primes
+
+print(prime_numbers(10))
