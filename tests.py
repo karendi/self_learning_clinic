832cc424d9de5f49583f90140fc74b1f4dc6dbdc
Sharonnjihia <sharonkarendi16@gmail.com>
2016-11-30 22:59:06 +0300

    added a function that checks that the list returned is not empty

--- tests.py
+++ tests.py
@@ -11,3 +11,8 @@ class Testprimenumbers(unittest.TestCase):
     #function to check if the numbers generated are actually primenumbers
     def test_prime_numbers(self):
         self.assertEqual(prime_numbers(5) , [2 , 3 ,5])
+
+    #function that checks if the list output is empty
+    def test_output(self):
+        results = prime_numbers(3)
+        self.assertTrue(results ,  "The output cannot be an empty list")
